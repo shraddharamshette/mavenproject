086bc7e55e3907f9b53e159a281ec08e62ac95b4
aishwarya ramshette <shraddharamshette@gmail.com>
2023-07-02 20:52:50 +0530

    Book Store project front End.

--- ELibrary/src/com/javatpoint/dao/DB.java
+++ ELibrary/src/com/javatpoint/dao/DB.java
@@ -1,41 +1,19 @@
 package com.javatpoint.dao;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DB {
-public static Connection getCon(){
+public static Connection getCon() throws ClassNotFoundException, SQLException{
 	Connection con=null;
-	try{
-//		Class.forName("oracle.jdbc.driver.OracleDriver");
-//		con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","system");
-		
-		Class.forName("com.mysql.jdbc.Driver");
-		con=DriverManager.getConnection("jdbc:mysql://localhost:3306/elibrary","root","root");
-		
-	}catch(Exception e){System.out.println(e);}
+//	try{
+//		Class.forName("com.mysql.cj.jdbc.Driver");
+//	    con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/elibrary","root","root");
+//		
+//	}catch(Exception e){System.out.println(e);}
+	Class.forName("com.mysql.cj.jdbc.Driver");
+    con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/elibrary","root","root");
 	return con;
 }
 }
-
-//
-//import java.sql.Connection;  
-//import java.sql.DriverManager;  
-//public class DB {  
-//         public static Connection getCon{
-//            String name,pass,url;  
-//            Connection con = null;  
-//            try {  
-//                Class.forName("com.mysql.jdbc.Driver");  
-//            url="jdbc:mysql://localhost:3306/elibrary";  
-//                name="root";  
-//                pass="";  
-//                con = DriverManager.getConnection(url,name,pass);  
-//                System.out.println("Connection created");  
-//                con.close();  
-//                System.out.println("Connection closed");  
-//                }  
-//                catch (Exception e) {  
-//                System.out.println(e.toString());  
-//            }  
-//    }  
-//}  
